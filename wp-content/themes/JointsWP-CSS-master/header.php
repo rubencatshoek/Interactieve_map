9817bf339e7bd5b414add30427fc25c0a073fb98
Ruben <rcatshoek@student.scalda.nl>
2018-11-22 13:05:03 +0100

    Added Google font

--- wp-content/themes/JointsWP-CSS-master/header.php
+++ wp-content/themes/JointsWP-CSS-master/header.php
@@ -28,6 +28,8 @@
 
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+
     <?php wp_head(); ?>
 
     <style>
